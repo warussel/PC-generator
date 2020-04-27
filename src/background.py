3dcc1e1f144dde6658e7f427bdada0fcebcd9f96
warussel <warussel@umich.edu>
2020-04-26 22:57:51 -0400

    Add backgrounds class

---
+++ src/background.py
@@ -0,0 +1,7 @@
+# Background class for character backgrounds
+
+class Background :
+    ''' A class representing a character's background '''
+
+    def __init__(self) :
+        print("TODO: implement me!")
