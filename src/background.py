8de7170f4e67e425fd910d1d437529a0592e6ea3
warussel <warussel@umich.edu>
2020-04-30 23:47:18 -0400

    More changes to background and race classes

--- src/background.py
+++ src/background.py
@@ -6,23 +6,25 @@ import sys
 class Background :
     ''' A class representing a character's background '''
 
-    def __init__(self) :
-        skills = []
-        tools = []
-        equipment = []
-        feature = ""
-        print("TODO: implement me!")
+    def __init__(self,name_in,skills_in,tools_in,equip_in,lang_in,feature_in) :
+        self.name = name_in
+        self.skills = skills_in
+        self.tools = tools_in
+        self.equipment = equip_in
+        self.feature = feature_in
+        self.num_lang = lang_in
+
 
 def read_backgrounds() :
-    ''' Read background data from an input JSON '''
+    ''' Function to read background data from an input JSON '''
     backgrounds = []
 
     f = open(os.path.join(sys.path[0], "backgrounds.json"), "r")
     data = json.load(f)
     
-    print("Printing backgrounds: ")
     for elt in data["backgrounds"] :
-        # TODO: Add backgrounds to array
-        print(elt["name"])
+        backgrounds.append(Background(elt["name"],elt["skills"],elt["tools"],elt["equipment"],elt["languages"],elt["feature"]))
+        print("Added: ", elt["name"])
+    return backgrounds
 
-read_backgrounds()
+sample = read_backgrounds()
