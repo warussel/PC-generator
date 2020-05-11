504773b80fbdccb8ea7c68a8e5ce7d9e37061bab
warussel <warussel@umich.edu>
2020-05-11 14:31:03 -0400

    Add jQuery

--- src/app.py
+++ src/app.py
@@ -7,17 +7,18 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def home() :
+    print("Render home page")
     return render_template("index.html")
 
-@app.route("/generate")
+@app.route("/generate", methods=['POST'])
 def generate() :
-
+    print("Received Character: ")
     _name = request.form['selectName']
     _class = request.form['selectClass']
     _race = request.form['selectRace']
     _bkground = request.form['selectBackground']
 
-    return ("Character: " + _name)
+    return json.dumps({"name": _name})
 
 if __name__ == "__main__" :
     app.run(debug=True) #TODO: set debug to false for deployment
--- src/static/js/form.js
+++ src/static/js/form.js
@@ -1,11 +1,15 @@
 // This Javascript function serves to submit a POST request to the server and display the resulting page
 $(function() {
-    $('#submit').click(function() {
-        alert("Clicked button");
+    $('button').click(function(){
+        console.log("Clicked button");
+        //var name = $('#selectName').val();
+        //var inputClass = $('#selectClass').val();
+        //var race = $('#selectRace').val();
+        //var background = $('#selectBackground').val();
         $.ajax({
             url: '/generate',
             data: $('form').serialize(),
-            type: 'GET',
+            type: 'POST',
             success: function(response) {
                 console.log(response);
             },
